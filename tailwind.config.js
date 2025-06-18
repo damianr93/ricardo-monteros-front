332a6d6bc969bf3d0e55254799fa5df32db0ab6b
Damian <a.damianrodriguez.93@gmail.com>
2025-06-18 11:43:45 -0300

    correcion de fuente

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,28 +9,26 @@ export default {
       colors: {
         // ————— COLORES PRIMARIOS —————
         primary: {
-          light: "#EFEAD8", // crema claro
-          muted: "#A49674", // topo grisáceo
-          DEFAULT: "#A81109", // rojo de marca
-          dark: "#000000", // negro puro
+          light: "#EFEAD8",
+          muted: "#A49674",
+          DEFAULT: "#A81109",
+          dark: "#000000",
         },
-
         // ————— COLORES SECUNDARIOS —————
         secondary: {
-          lightest: "#F9F7F2", // off-white
-          light: "#7F695D", // marrón claro
-          accent: "#E51E1F", // rojo vivo
-          dark: "#842020", // rojo oscuro
-          darkest: "#2A2728", // gris antracita
+          lightest: "#F9F7F2",
+          light: "#7F695D",
+          accent: "#E51E1F",
+          dark: "#842020",
+          darkest: "#2A2728",
         },
-
-        backgroundImage: {
-          "paper-pattern": "url('/assets/pattern-crema.svg')",
-        },
-        fontFamily: {
-          heading: ["Montserrat", "sans-serif"],
-          body: ["Poppins", "sans-serif"],
-        },
+      },
+      backgroundImage: {
+        "paper-pattern": "url('/assets/pattern-crema.svg')",
+      },
+      fontFamily: {
+        heading: ['Montserrat', 'sans-serif'], 
+        body: ["Poppins", "sans-serif"],
       },
     },
   },
